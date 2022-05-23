7e25c3e6212d0d33e4e28c3329593da5e632af85
Morgan Middleton <morgan.middleton@signify.co.nz>
2022-05-24 11:43:03 +1200

    chore: removed unnecessary plugins

--- translation/_config.php
+++ translation/_config.php
@@ -8,15 +8,7 @@ HtmlEditorConfig::get('cms')->enablePlugins(['TeReoPlugin' => '/vendor/signify-n
 HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'translate');
 
 //adding to the contextmenu config will override the defaults, must maintain the original configuration when adding
-HtmlEditorConfig::get('cms')->setOption('contextmenu', "contextTranslate addToDictionary test " . (HtmlEditorConfig::get('cms')->getOption('contextmenu')));
-//HtmlEditorConfig::get('cms')->setOption('init_instance_callback:', 'function (editor) {tinmyce.activeEditor.on(\'GetContent\', function (event) {console.log(\'right here\');event.content = restoreShortcodes(event.content);});');
-
-
-//can be removed
-HtmlEditorConfig::get('cms')->enablePlugins(['help', 'autolink', 'searchreplace']);
-HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'searchreplace');
-
-//HtmlEditorConfig::get('cms')->enablePlugins(['TeReoPlugin' => '/public/_resources/vendor/silverstripe/admin/thirdparty/tinymce/plugins/TeReoPlugin/TeReoPlugin.js']);
+HtmlEditorConfig::get('cms')->setOption('contextmenu', "contextTranslate addToDictionary " . (HtmlEditorConfig::get('cms')->getOption('contextmenu')));
 
 ShortcodeParser::get('default')->register('my_shortcode', ['ShortcodeHandler', 'MyShortCodeMethod']);
 ShortcodeParser::get('default')->register('TT', ['ShortcodeHandler', 'ShortMethod']);
