80a9b0e724e39e3c239c76a7d8b121dbeb64cb16
Morgan Middleton <morgan.middleton@signify.co.nz>
2022-08-01 13:06:08 +1200

    refactor: removed unused code

--- src/Controllers/PermissionController.php
+++ src/Controllers/PermissionController.php
@@ -2,23 +2,10 @@
 
 namespace Signify\TeReoTooltips\Controllers;
 
-use SilverStripe\Security\Permission;
 use SilverStripe\Security\PermissionProvider;
-use SilverStripe\Security\Security;
 
 class PermissionController implements PermissionProvider
 {
-    public function init()
-    {
-        parent::init();
-        if (!Permission::check('TOOLTIP_DICTIONARY_RIGHTS')) {
-            Security::permissionFailure();
-        }
-        if (!Permission::check('TOOLTIP_WORDPAIR_RIGHTS')) {
-            Security::permissionFailure();
-        }
-    }
-
     public function providePermissions()
     {
         return [
