ef0affa281905712b4b9d4422f50b1aa0a44cc63
Morgan Middleton <morgan.middleton@signify.co.nz>
2022-09-16 11:38:15 +1200

    fix: prevented content inside HTML tags from being matched by translate function DIA001-138

--- src/Services/LocalTranslator.php
+++ src/Services/LocalTranslator.php
@@ -88,7 +88,7 @@ class LocalTranslator implements TranslatorInterface
         foreach ($pairs as $word) {
             // /g is implicit in preg_replace.
             // regex look behind not supported in some browsers.
-            $regex = '/(?<![a-zA-Z0-9])(' . preg_quote($word->getField("Base")) . ')(?!\[\/TT])(?![a-zA-Z0-9])/';
+            $regex = '/(?<![a-zA-Z0-9])(' . preg_quote($word->getField("Base")) . ')(?!\[\/TT])(?![a-zA-Z0-9])(?![^<]*\>)/';
             $text = preg_replace($regex, "[TT]" . $word->getField('Base') . "[/TT]", $text);
         }
         return $text;
