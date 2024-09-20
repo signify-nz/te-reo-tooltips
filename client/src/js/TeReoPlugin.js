6c30a12ee933e78a14c03a91b98bfe6ccacd9b55
Owen Walker <owen.walker@signify.co.nz>
2024-09-20 12:40:26 +1200

    fix: Make Te Reo Tooltips plugin work with TinyMCE 6. SIGREO-57

--- client/src/js/TeReoPlugin.js
+++ client/src/js/TeReoPlugin.js
@@ -18,18 +18,18 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
   // credit: https://stackoverflow.com/questions/6234773/can-i-escape-html-special-chars-in-javascript
   function escapeHtml(text) {
     return text
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;');
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/'/g, '&quot;')
+      .replace(/'/g, '&#039;');
   }
 
   function regexQuote(str) {
     if (!str) {
       return '';
     }
-    return (str).replace(/[.?*+^$[\]\\(){}|-]/g, '\\$&');
+    return str.replace(/[.?*+^$[\]\\(){}|-]/g, '\\$&');
   }
 
   function encode(str) {
@@ -46,7 +46,7 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
     splitString = splitString[0].split('-');
     const decodedStr = [];
     for (let i = 0; i < splitString.length; i++) {
-    decodedStr[i] = String.fromCodePoint(splitString[i]);
+      decodedStr[i] = String.fromCodePoint(splitString[i]);
     }
     return decodedStr.join('');
   }
@@ -66,10 +66,11 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
       closeTag = '[/TT]';
       // TO-DO add regex for this instance? not currently used anywhere
     } else {
-      openTag = '<span class=\"TeReoTooltip\" style="text-decoration: underline 1px dashed;">';
+      openTag =
+        '<span class="TeReoTooltip" style="text-decoration: underline 1px dashed;">';
       closeTag = '</span>';
       openRegex = '(?<![a-zA-Z0-9])(';
-      closeRegex = ')(?!\[\/TT])(?![a-zA-Z0-9])(?![^<]*\>)';
+      closeRegex = ')(?![/TT])(?![a-zA-Z0-9])(?![^<]*>)';
     }
     let alteredContent = content;
 
@@ -79,10 +80,15 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
     dictionaryMap.forEach((value, key) => {
       const quotedKey = regexQuote(key);
       const regex = new RegExp(openRegex + quotedKey + closeRegex, 'gi');
-      alteredContent = alteredContent.replace(regex, (match) => openTag + encode(match) + closeTag);
+      alteredContent = alteredContent.replace(
+        regex,
+        (match) => openTag + encode(match) + closeTag
+      );
     });
     const regexDecode = new RegExp('(-%-%-).*?(-%-%-)', 'gi');
-    alteredContent = alteredContent.replace(regexDecode, (match) => decode(match));
+    alteredContent = alteredContent.replace(regexDecode, (match) =>
+      decode(match)
+    );
     return alteredContent;
   }
 
@@ -105,21 +111,23 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
       }
       const currentNode = walker.current();
       if (
-        currentNode.isEqualNode(rng.startContainer)
-        && currentNode.isEqualNode(rng.endContainer)
-        && currentNode.nodeType === 3
+        currentNode.isEqualNode(rng.startContainer) &&
+        currentNode.isEqualNode(rng.endContainer) &&
+        currentNode.nodeType === 3
       ) {
         foundStartNode = false;
-        const result = escapeHtml(currentNode.nodeValue.substr(0, rng.startOffset))
-        + checkForMatches(editor.selection.getContent(), false)
-        + escapeHtml(currentNode.nodeValue.substr(rng.endOffset));
+        const result =
+          escapeHtml(currentNode.nodeValue.substr(0, rng.startOffset)) +
+          checkForMatches(editor.selection.getContent(), false) +
+          escapeHtml(currentNode.nodeValue.substr(rng.endOffset));
         garbage = addHtmlToTextNode(currentNode, result);
       } else if (currentNode.nodeType === 3) {
-        if (currentNode.isEqualNode(rng.endContainer)
-        && foundStartNode) {
-          const result = checkForMatches(
-          currentNode.nodeValue.substr(0, rng.endOffset), false)
-          + currentNode.nodeValue.substr(rng.endOffset);
+        if (currentNode.isEqualNode(rng.endContainer) && foundStartNode) {
+          const result =
+            checkForMatches(
+              currentNode.nodeValue.substr(0, rng.endOffset),
+              false
+            ) + currentNode.nodeValue.substr(rng.endOffset);
           garbage = addHtmlToTextNode(currentNode, result);
           foundStartNode = false;
           finished = true;
@@ -127,8 +135,12 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
           const result = checkForMatches(currentNode.nodeValue, false);
           garbage = addHtmlToTextNode(currentNode, result);
         } else if (currentNode.isEqualNode(rng.startContainer)) {
-          const result = currentNode.nodeValue.substr(0, rng.startOffset)
-          + checkForMatches(currentNode.nodeValue.substr(rng.startOffset), false);
+          const result =
+            currentNode.nodeValue.substr(0, rng.startOffset) +
+            checkForMatches(
+              currentNode.nodeValue.substr(rng.startOffset),
+              false
+            );
           garbage = addHtmlToTextNode(currentNode, result);
           foundStartNode = true;
         }
@@ -144,7 +156,9 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
     if (!base || !destination) {
       tinymce.activeEditor.windowManager.alert('Cannot submit an empty field!');
     } else if (dictionaryMap.has(base)) {
-      tinymce.activeEditor.windowManager.alert(`This word already has a translation! (${dictionaryMap.get(base)})`);
+      tinymce.activeEditor.windowManager.alert(
+        `This word already has a translation! (${dictionaryMap.get(base)})`
+      );
     } else {
       const request = new XMLHttpRequest();
       const path = '/api/v1/dictionary/addWordPair/';
@@ -156,7 +170,7 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
         dictionaryID: id,
         baseWord: base,
         destinationWord: destination,
-        destinationAlternateWord: destinationAlternate
+        destinationAlternateWord: destinationAlternate,
       });
       request.send(body);
       request.onreadystatechange = function handleUpdateResponse() {
@@ -164,9 +178,13 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
           dictionaryMap.set(base, destination);
           tinymce.activeEditor.windowManager.close();
           if (destinationAlternate) {
-            tinymce.activeEditor.windowManager.alert(`Successfully added wordpair "${base}", "${destination}", "${destinationAlternate}".`);
+            tinymce.activeEditor.windowManager.alert(
+              `Successfully added wordpair '${base}', '${destination}', '${destinationAlternate}'.`
+            );
           } else {
-            tinymce.activeEditor.windowManager.alert(`Successfully added wordpair "${base}", "${destination}".`);
+            tinymce.activeEditor.windowManager.alert(
+              `Successfully added wordpair '${base}', '${destination}'.`
+            );
           }
         }
         if (this.readyState === 4 && this.status === 400) {
@@ -232,8 +250,8 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
           for (let i = 0; i < response.length; i++) {
             library.push({
               text: response[i].Title,
-              value: response[i].Title,
-              id: response[i].ID
+              value: response[i].ID.toString(),
+              id: response[i].ID,
             });
           }
         }
@@ -255,19 +273,27 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
     let restoration = null;
     const startOffset = 74;
     const endOffset = 7;
-    return content.replace(/(<span class="TeReoTooltip" style="text-decoration: underline 1px dashed;">)(.+?)(<\/span>)/g, (match) => {
-      restoration = match.slice(startOffset, match.length - endOffset);
-      dictionaryMap.forEach((value, key) => {
-        if (key.toLowerCase() ===
-        match.slice(startOffset, match.length - endOffset).toLowerCase()) {
-          restoration = `[TT]${match.slice(startOffset, match.length - endOffset)}[/TT]`;
-        }
-      });
-      if (!restoration) {
+    return content.replace(
+      /(<span class='TeReoTooltip' style='text-decoration: underline 1px dashed;'>)(.+?)(<\/span>)/g,
+      (match) => {
         restoration = match.slice(startOffset, match.length - endOffset);
+        dictionaryMap.forEach((value, key) => {
+          if (
+            key.toLowerCase() ===
+            match.slice(startOffset, match.length - endOffset).toLowerCase()
+          ) {
+            restoration = `[TT]${match.slice(
+              startOffset,
+              match.length - endOffset
+            )}[/TT]`;
+          }
+        });
+        if (!restoration) {
+          restoration = match.slice(startOffset, match.length - endOffset);
+        }
+        return restoration;
       }
-      return restoration;
-    });
+    );
   }
 
   // adding a zero width space on the end means that text written after
@@ -277,7 +303,8 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
   function replaceShortcodes(content) {
     const startOffset = 4;
     const endOffset = 5;
-    const openTag = '<span class=\"TeReoTooltip\" style="text-decoration: underline 1px dashed;">';
+    const openTag =
+      '<span class="TeReoTooltip" style="text-decoration: underline 1px dashed;">';
     const closeTag = '</span>';
     const zeroWidthSpace = '&#8203';
     let restoration = '';
@@ -285,22 +312,26 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
     return content.replace(/\[TT([^\]]*)\]([^\]]*)\[\/TT\]/g, (match) => {
       const spaceInserted = match.includes(zeroWidthSpace);
       if (dictionaryMap.size === 0) {
-        restoration = openTag
-        + match.slice(startOffset, match.length - endOffset)
-        + closeTag;
+        restoration =
+          openTag +
+          match.slice(startOffset, match.length - endOffset) +
+          closeTag;
         if (!spaceInserted) {
           restoration += zeroWidthSpace;
         }
         return restoration;
       }
       dictionaryMap.forEach((value, key) => {
-        if (key.toLowerCase() ===
-        match.slice(startOffset, match.length - endOffset).toLowerCase()) {
-          restoration = openTag
-          + match.slice(startOffset, match.length - endOffset)
-          + closeTag;
+        if (
+          key.toLowerCase() ===
+          match.slice(startOffset, match.length - endOffset).toLowerCase()
+        ) {
+          restoration =
+            openTag +
+            match.slice(startOffset, match.length - endOffset) +
+            closeTag;
           if (!spaceInserted) {
-          restoration += zeroWidthSpace;
+            restoration += zeroWidthSpace;
           }
         }
       });
@@ -328,52 +359,77 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
   // Credit to the searchReplace plugin for how to do this
   function addToDictMenu(selection) {
     editor.windowManager.open({
-      layout: 'flex',
-      pack: 'center',
-      align: 'center',
-      onClose() {
-        editor.focus();
-      },
-      onSubmit() {
+      title: 'Add to Dictionary',
+      body: {
+        type: 'panel',
+        layout: 'flex',
+        pack: 'center',
+        align: 'center',
+        items: [{
+          type: 'input',
+          name: 'Base',
+          size: 40,
+          label: 'Base',
+          id: 'BaseInputTextBox',
+          tooltip: 'An untranslated word/phrase',
+        },
+        {
+          type: 'input',
+          name: 'Destination',
+          size: 40,
+          label: 'Translation',
+          id: 'DestinationInputTextBox',
+          tooltip: 'A translated word/phrase',
+        },
+        {
+          type: 'input',
+          name: 'DestinationAlternate',
+          size: 40,
+          label: 'Alternate Translation',
+          id: 'DestinationAlternateInputTextBox',
+          tooltip: 'An alternate translated word/phrase',
+        },
+        {
+          type: 'listbox',
+          name: 'Dictionary',
+          size: 40,
+          label: 'Dictionary',
+          id: 'DictionaryDisplayComboBox',
+          tooltip: 'Select which dictionary you want to add to',
+          items: library,
+          onPostRender() {
+            // Select the first item by default, should get active?
+            this.value(library[0].value);
+          },
+        }],
       },
       buttons: [
         {
           text: 'Add',
-          subtype: 'primary',
-          onclick() {
-            // This is likely messier than it needs to be. can it
-            // handle special characters? is there any conversion?
-            const selectedID = library.find(o => o.text === document.getElementById('DictionaryDisplayComboBox-inp').value).id;
-            const alternateDest = document.getElementById('DestinationAlternateInputTextBox').value ? document.getElementById('DestinationAlternateInputTextBox').value : '';
-            newWordPair(document.getElementById('BaseInputTextBox').value.replace(/[\u200B-\u200D\uFEFF]/g, ''), document.getElementById('DestinationInputTextBox').value, alternateDest, selectedID);
-            }
+          type: 'submit',
+          buttonType: 'primary',
         },
-        { type: 'spacer', flex: 1 },
       ],
-      title: 'Add to Dictionary',
-      items: {
-        type: 'form',
-        padding: 20,
-        labelGap: 30,
-        spacing: 10,
-        items: [
-          { type: 'textbox', name: 'Base', size: 40, label: 'Base', value: selection.trim(), id: 'BaseInputTextBox', tooltip: 'An untranslated word/phrase' },
-          { type: 'textbox', name: 'Destination', size: 40, label: 'Translation', id: 'DestinationInputTextBox', tooltip: 'A translated word/phrase' },
-          { type: 'textbox', name: 'DestinationAlternate', size: 40, label: 'Alternate Translation', id: 'DestinationAlternateInputTextBox', tooltip: 'An alternate translated word/phrase' },
-          {
-            type: 'combobox',
-            name: 'Dictionary',
-            size: 40,
-            label: 'Dictionary',
-            id: 'DictionaryDisplayComboBox',
-            tooltip: 'Select which dictionary you want to add to',
-            values: library,
-            onPostRender() {
-              // Select the first item by default, should get active?
-              this.value(library[0].value);
-            },
-          },
-        ],
+      initialData: {
+        Base: selection.trim(),
+      },
+      onClose: () => {
+        editor.focus();
+      },
+      onSubmit: (api) => {
+        const data = api.getData();
+        // This is likely messier than it needs to be. can it
+        // handle special characters? is there any conversion?
+        const selectedID = parseInt(data.Dictionary, 10);
+        const base = data.Base.replace(/[\u200B-\u200D\uFEFF]/g, '');
+        const dest = data.Destination;
+        const alternateDest = data.DestinationAlternate;
+        newWordPair(
+          base,
+          dest,
+          alternateDest,
+          selectedID
+        );
       },
     });
   }
@@ -386,35 +442,35 @@ tinymce.PluginManager.add('TeReoPlugin', (editor, url) => {
     });
   }
 
-  editor.addMenuItem('addToDictionary', {
+  editor.ui.registry.addMenuItem('addToDictionary', {
     text: 'Add to Dictionary',
-    onclick() {
+    onAction: () => {
       const selection = editor.selection.getContent({ format: 'text' });
       addToDictMenu(selection);
     },
-    onPostRender: handleActiveState
+    onPostRender: handleActiveState,
   });
 
-  editor.addButton('translate', {
-    image: '/_resources/vendor/signify-nz/te-reo-tooltips/client/dist/img/globe-light.svg',
+  editor.ui.registry.addButton('translate', {
+    icon: 'language',
     tooltip: 'Translate content',
-    onclick() {
+    onAction: () => {
       bookmark = tinymce.activeEditor.selection.getBookmark(2, true);
       editor.undoManager.add();
       translateThroughAPI(editor.getContent());
       tinymce.activeEditor.fire('change');
     },
-    onPostRender: handleActiveState
+    onPostRender: handleActiveState,
   });
 
   // repeated translations cause issues, fix by adding validation in checkForMatches?
-  editor.addMenuItem('contextTranslate', {
+  editor.ui.registry.addMenuItem('contextTranslate', {
     text: 'Translate',
-    onclick() {
+    onAction: () => {
       bookmark = tinymce.activeEditor.selection.getBookmark(2, true);
       treeWalk(editor.selection.getRng());
       tinymce.activeEditor.fire('change');
     },
-    onPostRender: handleActiveState
+    onPostRender: handleActiveState,
   });
 });
